68f2f3fd01e6e025290b04e24260685b491e4854
renjumenonr <r.renjumenon@gmail.com>
2021-03-13 19:06:08 +0800

    Implement network configuration impl for network ssupport

---
+++ app/src/main/java/com/gelatotest/gelatophotos/support/NetworkConfigurationImpl.kt
@@ -0,0 +1,23 @@
+package com.gelatotest.gelatophotos.support
+
+import android.content.Context
+import com.gelatotest.network.support.NetworkConfiguration
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.io.File
+
+class NetworkConfigurationImpl(private val context: Context) : NetworkConfiguration {
+
+    override fun mainThreadScheduler(): Scheduler = AndroidSchedulers.mainThread()
+
+    override fun ioScheduler(): Scheduler = Schedulers.io()
+
+    override fun getHost() = "https://picsum.photos/"
+
+    override fun getCacheDir(): File = context.cacheDir
+
+    override fun getCacheSize(): Long = 10 * 1024 * 1024
+
+    override fun getTimeoutSeconds(): Long = 5
+}
